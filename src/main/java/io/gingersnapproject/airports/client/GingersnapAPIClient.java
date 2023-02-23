237bde78891d7bc20e54d26fe96aca0e63643fde
Pedro Z <pzapataf@gmail.com>
2023-02-23 22:31:54 +0100

    First Demo Dashboard UI with Patternfly (wip)

--- src/main/java/io/gingersnapproject/airports/client/GingersnapAPIClient.java
+++ src/main/java/io/gingersnapproject/airports/client/GingersnapAPIClient.java
@@ -1,14 +1,12 @@
 package io.gingersnapproject.airports.client;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-
-import io.gingersnapproject.airports.model.Flight;
 import org.eclipse.microprofile.rest.client.annotation.RegisterClientHeaders;
 import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
 
-import io.gingersnapproject.airports.DashboardFlightDTO;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.Response;
 
 @RegisterRestClient(configKey = "gingersnap-api")
 @RegisterClientHeaders
@@ -16,9 +14,9 @@ public interface GingersnapAPIClient {
 
    @GET
    @Path("/us-flight/{code}")
-   String flight(@PathParam("code") String code);
+   Response flight(@PathParam("code") String code);
 
    @GET
-   @Path("/dashboard-departure/{code}")
-   String departure(@PathParam("code") String code);
+   @Path("/rules")
+   Response flightsQuery(@PathParam("query") String code);
 }
