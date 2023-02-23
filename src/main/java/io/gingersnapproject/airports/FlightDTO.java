51babbc4cfc594c35b458cc23c4741554d083a09
Pedro Z <pzapataf@gmail.com>
2023-02-23 22:31:54 +0100

    First Demo Dashboard UI with Patternfly (wip)

--- src/main/java/io/gingersnapproject/airports/FlightDTO.java
+++
@@ -1,9 +0,0 @@
-package io.gingersnapproject.airports;
-
-public class FlightDTO {
-   public String code;
-   public String name;
-   public String state;
-   public String destinationCity;
-   public String scheduleTime;
-}
--- src/main/java/io/gingersnapproject/airports/FlightsResource.java
+++ src/main/java/io/gingersnapproject/airports/FlightsResource.java
@@ -10,13 +10,7 @@ import org.eclipse.microprofile.rest.client.inject.RestClient;
 
 import javax.inject.Inject;
 import javax.transaction.Transactional;
-import javax.ws.rs.GET;
-import javax.ws.rs.NotFoundException;
-import javax.ws.rs.PUT;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.util.List;
@@ -33,30 +27,14 @@ public class FlightsResource {
    @RestClient
    GingersnapAPIClient gingersnapAPIClient;
 
-
+/*
    @GET
    @Produces(MediaType.APPLICATION_JSON)
    public CompletionStage<String> checkResourceUp() {
       return CompletableFuture.completedFuture("Flights resource is up");
    }
 
-   @GET
-   @Produces(MediaType.APPLICATION_JSON)
-   @Path("departures/{day}")
-   @Blocking
-   public List<Flight> flights(@PathParam("day") Integer day, @QueryParam("iata") Optional<String> iataOpt) {
-      Log.infof("Request flights for day %d and destination %s", day, iataOpt);
-      if (iataOpt.isPresent()) {
-         String iata = iataOpt.get();
-         Airport airport = Airport.<Airport>find("iata", iata)
-               .firstResultOptional().orElseThrow(() -> new NotFoundException("Airport not found " + iata));
-         return  Flight.find("destination = :destination and dayOfWeek = :dayOfWeek",
-                     with("destination", airport).and("dayOfWeek", day).map())
-               .list();
-      }
 
-      return Flight.find("dayOfWeek",day).list();
-   }
 
    @GET
    @Produces(MediaType.APPLICATION_JSON)
@@ -89,20 +67,39 @@ public class FlightsResource {
 
       return Response.notModified().build();
    }
+*/
+   /*
 
-   @GET
-   @Produces(MediaType.APPLICATION_JSON)
-   @Blocking
-   @Path("cache/departures/get/{code}")
-   public String flightFromCache(@PathParam("code") String code) {
-      return gingersnapAPIClient.departure(code);
-   }
+      Gingersnap REST calls
+
+    */
 
    @GET
    @Produces(MediaType.APPLICATION_JSON)
    @Blocking
    @Path("cache/{code}")
-   public String flights(@PathParam("code") String code) {
+   public Response flights(@PathParam("code") String code) {
       return gingersnapAPIClient.flight(code);
+   }
+
+   @GET
+   @Produces(MediaType.APPLICATION_JSON)
+   @Path("departures/{day}")
+   @Blocking
+   public List<Flight> flights(@PathParam("day") Integer day, @QueryParam("iata") Optional<String> iataOpt) {
+      Log.infof("Request flights for day %d and destination %s", day, iataOpt);
+      if (iataOpt.isPresent()) {
+         String iata = iataOpt.get();
+         Airport airport = Airport.<Airport>find("iata", iata)
+                 .firstResultOptional().orElseThrow(() -> new NotFoundException("Airport not found " + iata));
+         return  Flight.find("destination = :destination and dayOfWeek = :dayOfWeek",
+                         with("destination", airport).and("dayOfWeek", day).map())
+                 .list();
+      }
+
+      return Flight.find("dayOfWeek",day).list();
+
+      // TODO: Replace with
+      //return gingersnapAPIClient.flightsQuery("SELECT * FROM us-flight");
    }
 }
